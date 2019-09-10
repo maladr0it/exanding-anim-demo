884e3d93d15cea606fd12f5c442b79688a095cbd
Lachlan Bradford <bradford.lachlan@gmail.com>
2019-09-10 22:53:31 +0900

    add basic example

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+
+import { lorem } from "./lorem";
+import { createKeyframes } from "./createKeyframes";
+import { ReactComponent as DownSvg } from "./assets/angle-down-solid.svg";
+import { ReactComponent as UpSvg } from "./assets/angle-up-solid.svg";
+
+import "./App.css";
 
 const App: React.FC = () => {
+  const keyframesRef = useRef<Keyframe[] | null>(null);
+  const inverseKeyframesRef = useRef<Keyframe[] | null>(null);
+  const touchedRef = useRef(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const contentRef = useRef<HTMLParagraphElement>(null);
+
+  const [open, setOpen] = useState(false);
+
+  const handleToggle = () => {
+    setOpen(prev => !prev);
+  };
+
+  useEffect(() => {
+    if (containerRef.current && contentRef.current) {
+      const containerRect = containerRef.current.getBoundingClientRect();
+      const contentRect = contentRef.current.getBoundingClientRect();
+      const { keyframes, inverseKeyframes } = createKeyframes(
+        containerRect,
+        contentRect
+      );
+      keyframesRef.current = keyframes;
+      inverseKeyframesRef.current = inverseKeyframes;
+    }
+  }, []);
+
+  useEffect(() => {
+    if (
+      touchedRef.current &&
+      containerRef.current &&
+      contentRef.current &&
+      keyframesRef.current &&
+      inverseKeyframesRef.current
+    ) {
+      containerRef.current.animate(keyframesRef.current, {
+        duration: 150,
+        fill: "forwards",
+        direction: open ? "normal" : "reverse"
+      });
+      contentRef.current.animate(inverseKeyframesRef.current, {
+        duration: 150,
+        fill: "forwards",
+        direction: open ? "normal" : "reverse"
+      });
+    }
+    touchedRef.current = true;
+  }, [open]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div ref={containerRef} className="Expander">
+        <div ref={contentRef} className="Expander-content">
+          <button className="Expander-toggleButton" onClick={handleToggle}>
+            {open ? <UpSvg /> : <DownSvg />}
+          </button>
+          <p className="Expander-text">{lorem}</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/lorem.ts
@@ -0,0 +1,5 @@
+// export const lorem =
+//   "Lorem ipsum dolor sit amet, consectetuer ligula consequuntur, vestibulum imperdiet donec consequat proin enim gravida, dui at purus, interdum potenti consequat. Diam nec dui, tortor velit ut nam pretium vel, aliquam amet, quam erat cras nec in quam magna, massa elit mauris. Suspendisse rhoncus lorem dolor posuere, aliquam felis et, odio sed sem diam volutpat dolor in, quis at pellentesque odio condimentum sed, molestie tortor. Lacus integer praesent ut, ipsum amet in et. Neque vel ut commodo vivamus vivamus molestie, eget sapien, vitae et quam torquent pede luctus, dignissim cursus lobortis nostrum donec, vitae tortor consectetuer dui elementum.";
+
+export const lorem =
+  "Lorem ipsum dolor sit amet, consectetuer ligula consequuntur";
